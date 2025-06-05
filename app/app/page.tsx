7c2ea399cbab4a5e83615382509498e7da6c7bf3
Cribappco <cribapp.co@gmail.com>
2025-06-05 08:37:31 -0400

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>Crib App</h1>
+      <p>This is the homepage. You probably meant to go to <code>/verify</code>.</p>
+    </main>
+  );
+}
